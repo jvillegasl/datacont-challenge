e4edc37190fb174c585b7b43b66b1c2567f551ba
jvillegasl <jvillegasl@uni.pe>
2023-08-18 15:45:36 -0500

    methods get books and book implemented

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -1,7 +1,21 @@
+import { Book } from "@/models";
 import { RequestHandler } from "express";
 
 export class BooksController {
     public static getBooks: RequestHandler = (req, res) => {
-        res.send([1, 2, 3, 4]);
+        const books = Book.all();
+
+        return res.send(books);
+    };
+
+    public static getBook: RequestHandler = (req, res) => {
+        const id = req.params.id;
+        const book = Book.findById(id);
+
+        if (!book) {
+            return res.sendStatus(404);
+        }
+
+        return res.send(book);
     };
 }
