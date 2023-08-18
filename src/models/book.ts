edec0515e6e4b93f25697834b2588d4431d8c00b
jvillegasl <jvillegasl@uni.pe>
2023-08-18 15:46:19 -0500

    book model implemented

---
+++ src/models/book.ts
@@ -0,0 +1,29 @@
+import fs from "fs";
+
+export interface IBook {
+    id: number;
+    title: string;
+    description: string;
+    author: string;
+    price: number;
+}
+
+export class Book {
+    public static all() {
+        const filePath = "src/data/books.json";
+        const jsonData = fs.readFileSync(filePath, "utf-8");
+        const books: IBook[] = JSON.parse(jsonData);
+
+        return books;
+    }
+
+    public static findById(id: string) {
+        const filePath = "src/data/books.json";
+        const jsonData = fs.readFileSync(filePath, "utf-8");
+        const books: IBook[] = JSON.parse(jsonData);
+
+        const book = books.find((t) => t.id.toString() === id);
+
+        return book;
+    }
+}
