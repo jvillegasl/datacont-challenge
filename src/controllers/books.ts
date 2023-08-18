691825fbc46bc61a7fbafd672dd4509ab69e1f54
jvillegasl <jvillegasl@uni.pe>
2023-08-18 18:05:28 -0500

    error handling implemented

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -3,19 +3,30 @@ import { RequestHandler } from "express";
 
 export class BooksController {
     public static getBooks: RequestHandler = (req, res) => {
-        const books = Book.all();
+        try {
+            const books = Book.all();
 
-        return res.send(books);
+            return res.send(books);
+        } catch (error) {
+            console.error(error);
+
+            return res.sendStatus(500);
+        }
     };
 
     public static getBook: RequestHandler = (req, res) => {
         const id = req.params.id;
-        const book = Book.findById(id);
 
-        if (!book) {
-            return res.sendStatus(404);
+        try {
+            const book = Book.findById(id);
+
+            if (!book) return res.sendStatus(404);
+
+            return res.send(book);
+        } catch (error) {
+            console.error(error);
+
+            return res.sendStatus(500);
         }
-
-        return res.send(book);
     };
 }
