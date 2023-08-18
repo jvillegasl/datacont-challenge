3069d7c43897ba9b470547d4d9b350c046ee2fde
jvillegasl <jvillegasl@uni.pe>
2023-08-18 18:17:01 -0500

    updated url

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
 import express from "express";
-import { Request, Response } from "express";
 import { apiRouter } from "./routes";
 
 const PORT = 5000;
 
 const app = express();
 
-app.get("/", function (req: Request, res: Response) {
-    return res.send(
-        "Welcome to ExpressJS + Typescript Datacont Challenge server"
-    );
-});
-
-app.use("/api", apiRouter);
+app.use("/", apiRouter);
 
 app.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:3000`);
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
