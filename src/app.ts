339acf6a8eea48080f29119c6a09d5ef8aa792f1
jvillegasl <jvillegasl@uni.pe>
2023-08-18 16:31:20 -0500

    server port changed to 5000

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import express from "express";
 import { Request, Response } from "express";
 import { apiRouter } from "./routes";
 
+const PORT = 5000;
+
 const app = express();
 
 app.get("/", function (req: Request, res: Response) {
-    return res.send("Welcome to ExpressJS + Typescript server");
+    return res.send(
+        "Welcome to ExpressJS + Typescript Datacont Challenge server"
+    );
 });
 
 app.use("/api", apiRouter);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:3000`);
 });
